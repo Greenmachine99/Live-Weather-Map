4e215bb9c9720039e6326b9ac3594ea5df98fa8b
Greenmachine <sam.o.groen99@gmail.com>
2023-10-18 20:32:21 +0200

    APIkeys.js removed

--- src/features/APIkeys.js
+++
@@ -1,5 +0,0 @@
-// Set OpenWeatherMap API Key
-export const OpenWeatherKey = '2c9becb8c70dcbbad91b1e843c03bcaa';
-
-// GitHub Token 
-const GitHubToken = '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';
